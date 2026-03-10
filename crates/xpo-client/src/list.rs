a90ec2b01c2691751d8f85d31ef899c4e8439e97
Chingiz Mammadov <info@chz.dev>
2026-03-11 01:19:43 +0400

    feat(client): add CLI args for --password, --ttl, --wait, xpo list

---
+++ crates/xpo-client/src/list.rs
@@ -0,0 +1,3 @@
+pub async fn run(_json: bool) -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
---
+++ crates/xpo-client/src/wait.rs
@@ -0,0 +1,6 @@
+pub async fn wait_for_port(
+    _port: u16,
+    _timeout: std::time::Duration,
+) -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
